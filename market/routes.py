583bee09d3b3cff1e3ffc31d967d7d23c942fb7a
Julian Aguilar <julianaguilar98@gmail.com>
2021-08-20 00:04:27 -0500

    added warning messages that display on web app

--- market/routes.py
+++ market/routes.py
@@ -1,5 +1,5 @@
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
 from market.models import Item, User
 from market.forms import RegisterForm
 from market import db
@@ -26,6 +26,7 @@ def register_page():
         return redirect(url_for('market_page'))
     if form.errors != {}: #If there are not errors from the validations
         for err_msg in form.errors.values():
-            print(f'There was an error with creating a user: {err_msg}')
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
 
     return render_template('register.html', form=form)
