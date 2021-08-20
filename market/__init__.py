42c5567ac6dab2945e3e97bd80caf8d3e1a39762
Julian Aguilar <julianaguilar98@gmail.com>
2021-08-19 20:03:15 -0500

    added register.html and forms.py in order to create register website

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '557982242aadc2f48524c3e6'
+
 db = SQLAlchemy(app)
 
 from market import routes
--- market/models.py
+++ market/models.py
@@ -16,5 +16,5 @@ class Item(db.Model):
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
-    def __repr__(self)
+    def __repr__(self):
         return f'Item {self.name}'
