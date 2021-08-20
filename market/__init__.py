a50a67ed676aebf98500fc249fd11f37dca816cd
Julian Aguilar <julianaguilar98@gmail.com>
2021-08-20 01:12:48 -0500

    added ability to login, and updated UI

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '557982242aadc2f48524c3e6'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app
 from flask import render_template, redirect, url_for, flash, get_flashed_messages
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 from market import db
 
 @app.route('/')
@@ -20,7 +20,7 @@ def register_page():
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                               email_address=form.email_address.data,
-                              password_hash=form.password1.data)
+                              password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
@@ -30,3 +30,8 @@ def register_page():
 
 
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', form=form)
