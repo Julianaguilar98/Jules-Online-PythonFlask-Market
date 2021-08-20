8de634196187251073dae1d1c38591a0d49bd507
Julian Aguilar <julianaguilar98@gmail.com>
2021-08-20 04:04:39 -0500

    added more login authentication for username, added budget

--- market/__init__.py
+++ market/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
@@ -8,5 +9,5 @@ app.config['SECRET_KEY'] = '557982242aadc2f48524c3e6'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
+login_manager = LoginManager(app)
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -3,6 +3,8 @@ from flask import render_template, redirect, url_for, flash, get_flashed_message
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
+from flask_login import login_user
+from flask_fontawesome import FontAwesome
 
 @app.route('/')
 @app.route('/home')
@@ -34,4 +36,15 @@ def register_page():
 @app.route('/login', methods=['GET','POST'])
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and Password are not a match! Try again', category='danger')
+
     return render_template('login.html', form=form)
